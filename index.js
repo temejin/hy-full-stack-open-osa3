0ecc4c6f352ee757e5691e31634a464d0708de07
teme <93370315+temejin@users.noreply.github.com>
2023-10-12 13:18:23 +0300

    deployaus internetiin

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan('tiny'))
 morgan.token('post', (req,res) => {return JSON.stringify(res.body)})
